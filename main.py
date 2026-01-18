248788d254381bdac89a18169f086731395d208b
Arjun-MS2005 <arjunms2005@gmail.com>
2026-01-18 10:31:26 +0530

    changing the main to work with github actions

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from fetch_solved import *
-from email_sender import *
-from picker import *
+from fetch_solved import fetch_solved_questions
+from email_sender import send_daily_revision_email
+from picker import pick
+import json
+
 
 if __name__ == "__main__":
-    fetch_solved_questions()
-    send_daily_revision_email(pick())
+    data = fetch_solved_questions()
+    if not data: 
+        print("No data received, aborting")
+        exit()
+    with open("solved_with_dates.json" , "w") as f: 
+        json.dump(data, f)
+
+    selected_questions = pick()
+    
+    send_daily_revision_email(selected_questions)
